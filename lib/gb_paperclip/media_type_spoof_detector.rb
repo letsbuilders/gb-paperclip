69da7e46f203f698e91f4340cb381bb9ef083a2b
Kacper Kawecki <kacper@geniebelt.com>
2015-09-28 14:16:30 +0200

    Fixed error in media spoof detector

--- lib/gb_paperclip/media_type_spoof_detector.rb
+++ lib/gb_paperclip/media_type_spoof_detector.rb
@@ -8,7 +8,7 @@ module Paperclip
       if content_types_from_name.count > 0
         spoofed = old_spoofed.bind(self).()
         if defined? Rails
-          Rails.logger.fatal "MIME Spoofed file alert! file: #{filename_extension} detected ad #{calculated_media_type} but it should be: #{supplied_file_content_types}" if spoofed
+          Rails.logger.fatal "MIME Spoofed file alert! file: #{filename_extension} detected ad #{calculated_media_type} but it should be: #{supplied_content_type}" if spoofed
         end
         spoofed
       else
--- lib/gb_paperclip/version.rb
+++ lib/gb_paperclip/version.rb
@@ -1,3 +1,3 @@
 module GBPaperclip
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 end
