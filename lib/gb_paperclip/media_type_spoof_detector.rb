e72e85ad0c85980d40509d0041a9a3ebd24fa24b
Kacper Kawecki <kacper@geniebelt.com>
2016-02-02 18:42:55 +0100

    Fixed logs for spoof detector

--- lib/gb_paperclip/media_type_spoof_detector.rb
+++ lib/gb_paperclip/media_type_spoof_detector.rb
@@ -6,7 +6,7 @@ module Paperclip
     def spoofed?
       if has_name? && has_extension? && calculated_content_type_mismatch? && content_types_from_name.count > 0
         if media_type_mismatch? && mapping_override_mismatch?
-          Paperclip.log("Content Type Spoof probablity: Filename #{File.basename(@name)}, detected as #{calculated_content_type} but should be #{content_types_from_name.map(&:content_type).join(', ')}")
+          Paperclip.log("Content Type Spoof detected: Filename #{File.basename(@name)}, detected as #{calculated_content_type} but should be #{content_types_from_name.map(&:content_type).join(', ')}")
           true
         else
           Paperclip.log("Content Type Spoof probablity: Filename #{File.basename(@name)}, detected as #{calculated_content_type} but should be #{content_types_from_name.map(&:content_type).join(', ')}")
