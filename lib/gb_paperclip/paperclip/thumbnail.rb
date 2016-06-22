31b4ab6fbb124192269b6947df9768a5b1f30008
Kacper Kawecki <kacper@geniebelt.com>
2016-06-22 16:16:09 +0200

    Fixed wrong style info if @whiny

--- lib/gb_paperclip/paperclip/thumbnail.rb
+++ lib/gb_paperclip/paperclip/thumbnail.rb
@@ -8,9 +8,30 @@ module Paperclip
       end
 
       def make
-        result = super
-        @attachment.finished_processing @style if @attachment && @style
-        result
+        src = @file
+        dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
+        dst.binmode
+
+        begin
+          parameters = []
+          parameters << source_file_options
+          parameters << ":source"
+          parameters << transformation_command
+          parameters << convert_options
+          parameters << ":dest"
+
+          parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
+
+          success = convert(parameters, :source => "#{File.expand_path(src.path)}#{'[0]' unless animated?}", :dest => File.expand_path(dst.path))
+          @attachment.finished_processing @style if @attachment && @style
+        rescue Cocaine::ExitStatusError => e
+          @attachment.failed_processing @style if @attachment && @style
+          raise Paperclip::Error, "There was an error processing the thumbnail for #{@basename}" if @whiny
+        rescue Cocaine::CommandNotFoundError => e
+          @attachment.failed_processing @style if @attachment && @style
+          raise Paperclip::Errors::CommandNotFoundError.new("Could not run the `convert` command. Please install ImageMagick.")
+        end
+        dst
       end
     end
 
