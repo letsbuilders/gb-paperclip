7a0615a7b4b62336354fa0ea2f63d50e4ed17b5d
Kacper Kawecki <kacper@letsbuild.com>
2022-05-02 16:40:40 +0200

    Rubocop fixes

--- lib/gb_paperclip/paperclip/validators/quarantine.rb
+++ lib/gb_paperclip/paperclip/validators/quarantine.rb
@@ -1,13 +1,19 @@
+# frozen_string_literal: true
+
 require 'kt-paperclip'
+
 module Paperclip
   module Validators
+    # Validator which is moving suspected spoofed files to quarantine folder
     class MediaTypeSpoofDetectionValidator
       def validate_each(record, attribute, value)
         adapter = Paperclip.io_adapters.for(value)
-        if Paperclip::MediaTypeSpoofDetector.using(adapter, value.original_filename, value.content_type).spoofed?
-          record.errors.add(attribute, :spoofed_media_type)
-          move_to_quarantine adapter, value.original_filename
-        end
+        return unless Paperclip::MediaTypeSpoofDetector
+                      .using(adapter, value.original_filename, value.content_type)
+                      .spoofed?
+
+        record.errors.add(attribute, :spoofed_media_type)
+        move_to_quarantine adapter, value.original_filename
       end
 
       def move_to_quarantine(adapter, original_filename)
@@ -17,8 +23,8 @@ module Paperclip
 
       def quarantine_directory
         path = 'tmp/quarantine'
-        unless Dir.exists? path
-          FileUtils.mkdir 'tmp' unless Dir.exists? 'tmp'
+        unless Dir.exist? path
+          FileUtils.mkdir 'tmp' unless Dir.exist? 'tmp'
           FileUtils.mkdir path
         end
         path
